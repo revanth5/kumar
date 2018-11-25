42c5522727c3508d03bda47af5cf0f36d9cf2c65
revanth5 <44646933+revanth5@users.noreply.github.com>
2018-11-25 17:47:14 +0000

    Add files via upload

--- task2.py
+++ task2.py
@@ -3,9 +3,9 @@ from scapy.all import *
 
 def main():
 	
-	dest_ip="192.168.0.228"
-	src_ip="192.168.0.152"
-	dest_mac="08:00:27:ba:f4:a2"
+	dest_ip="192.168.0.1"
+	src_ip="192.168.0.73"
+	dest_mac="38:43:7d:a9:0d:ae"
 	src_mac="50:3e:aa:85:4e:9e"
 	send_icmpRedirect_packet(src_ip,dest_ip,src_mac,dest_mac)
 
